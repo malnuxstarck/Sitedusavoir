c25e277b2ef65ee9a47576e69db3fe5a853a5b8c
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2016-10-08 18:30:35 +0200

    Suppression des sessions actives

--- confirm.php
+++ confirm.php
@@ -16,19 +16,18 @@ session_start();
 
 if($user AND $user['token'] == $token)
 {
-	session_start();
+	if(session_status() == PHP_SESSION_NONE)
+	  session_start();
 
-	$_SESSION['pseudo'] = $data['membre_pseudo'];
-	$_SESSION['level'] = $data['membre_rang'];
-	$_SESSION['id'] = $data['membre_id'];
 	
-	$_SESSION['flash']['success'] =" votre compte a ete validee";
+	
+	$_SESSION['flash']['success'] =" votre compte a ete validee ! Vous pouvez vous connecter";
 
 	$req = $bdd->prepare('UPDATE membres SET token = NULL, membre_inscrit = NOW() WHERE membre_id = :id ');
 
 	$req->execute(array('id' => $id));
 
-    header('Location:membre/voirmonprofil.php');
+    header('Location:connexion.php');
 }
 else
 {
