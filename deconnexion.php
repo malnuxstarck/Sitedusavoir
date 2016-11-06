0c28d1b5408c52f5240c675b92e173cdf58cf5ef
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2016-11-06 10:49:16 +0100

    deconnecter de la ligne

--- deconnexion.php
+++ deconnexion.php
@@ -6,9 +6,15 @@ session_destroy();
 unset($_SESSION['flash']);
 
 setcookie('souvenir',NULL,-1);
-
+include("./includes/identifiants.php");
 include_once('./includes/debut.php');
 include_once('./includes/menu.php');
+
+$query=$bdd->prepare('DELETE FROM forum_whosonline WHERE online_id=
+:id');
+$query->bindValue(':id',$id,PDO::PARAM_INT);
+$query->execute();
+$query->CloseCursor();
 
 
 if ($id == 0) 
