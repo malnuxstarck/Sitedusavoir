cf6261fbac9db5fbf5e0f5ef7b8902d5cdfa8688
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2016-11-11 17:04:22 +0100

    Indentation du code et correction de l'activation de la session

--- index.php
+++ index.php
@@ -3,144 +3,144 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-     <meta charset="utf-8"/>
-	<title>Site du savoir | Sitedesavoir.com</title>
-	<meta name="author" content="MalnuxStrck"/>
-	<meta name="description" content="site de partages de connaissances"/>
-	<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	<link rel="icon" href="logo.png"/>
+      <head>
+            <meta charset="utf-8"/>
+          	<title>Site du savoir | Sitedesavoir.com</title>
+          	<meta name="author" content="MalnuxStrck"/>
+          	<meta name="description" content="site de partages de connaissances"/>
+          	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+          	<link rel="icon" href="logo.png"/>
 
-</head>
-<?php 
-    
-            if(isset($_SESSION['level'],$_SESSION['id'],$_SESSION['pseudo']))
-            {
-            $lvl = (int)$_SESSION['level'];
-            $id = (int)$_SESSION['id'];
-            $pseudo = $_SESSION['pseudo'];
-            }
-            else
-            {
-            $lvl = 1;
-            $id = 0;
-            $pseudo = '';
-            }
-
-            include_once('includes/menu.php');
-            include_once('includes/identifiants.php');
-   ?>
-    
-   
-
-    <?php if ($id)
-    {
-
-      $req = $bdd->prepare('SELECT membre_pseudo,membre_avatar,membre_email FROM membres WHERE membre_id = :id');
-      $req->execute(array('id' => $id));
-      $data = $req->fetch();
-
-
-      echo ' <aside id="aconnexion"> 
-      <h2 id="connexion">'.$data['membre_pseudo'].' </h2>
-       
-          <div id="avatar">
-             <img src="images/avatars/'.$data['membre_avatar'].'" alt="Pas davatar"/>
-          </div>
-
-          <p><a href="mailto:'.$data['membre_email'].'">'.$data['membre_email'].'</a>
-          <p>
+      </head>
+      <?php 
           
-          <p> <a href="membre/voirmonprofil.php?id='.$id.'"> Voir son profil </a></p>
+                  if(isset($_SESSION['level'],$_SESSION['id'],$_SESSION['pseudo']))
+                  {
+                  $lvl = (int)$_SESSION['level'];
+                  $id = (int)$_SESSION['id'];
+                  $pseudo = $_SESSION['pseudo'];
+                  }
+                  else
+                  {
+                  $lvl = 1;
+                  $id = 0;
+                  $pseudo = '';
+                  }
 
-          <p> <a href="membre/editerprofil.php?id='.$id.'">Editer profil </a></p>
-          <p>
-              <a href="deconnexion.php"> Se deconnecter </a>
-          </p>
+                  include_once('includes/menu.php');
+                  include_once('includes/identifiants.php');
+         ?>
+          
+         
 
-          </aside>';
-}
+          <?php if ($id)
+          {
 
- else{
-  ?>
-      <aside id="aconnexion">
+            $req = $bdd->prepare('SELECT membre_pseudo,membre_avatar,membre_email FROM membres WHERE membre_id = :id');
+            $req->execute(array('id' => $id));
+            $data = $req->fetch();
 
-        <h2 id="connexion"> Connexion </h2>
-        <form id="seconnecter" method="POST" action="connexion.php">
-        	<p>
-        	   <input type="text" name="pseudo" placeholder="Pseudo"/>
-        	</p>
 
-        	<p>
-                <input type="password" name="password" placeholder="Mot de passe"/>
-        	</p>
+            echo ' <aside id="aconnexion"> 
+            <h2 id="connexion">'.$data['membre_pseudo'].' </h2>
+             
+                <div id="avatar">
+                   <img src="images/avatars/'.$data['membre_avatar'].'" alt="Pas davatar"/>
+                </div>
 
-        	<p>
-        	   <input type="checkbox" name="souvenir"/> Se souvenir de moi 
-        	</p>
+                <p><a href="mailto:'.$data['membre_email'].'">'.$data['membre_email'].'</a>
+                <p>
+                
+                <p> <a href="membre/voirmonprofil.php?id='.$id.'"> Voir son profil </a></p>
 
-        	<p>
-               <input type="submit" value="Se connecter"/>
-        	</p>
-        </form>
-          <p id="oubli">
-             <a href="oublie.php">Mot de passe perdu </a>
-          </p>
-          <p id="nouveau">
-              <a href="register.php"> Nouveau , Inscivez Vous</a>
-          </p>
+                <p> <a href="membre/editerprofil.php?id='.$id.'">Editer profil </a></p>
+                <p>
+                    <a href="deconnexion.php"> Se deconnecter </a>
+                </p>
 
-           </aside>
-        <?php 
+                </aside>';
       }
-        ?>  
-        
-   
 
-    <div id="arianepresentation">
+       else{
+        ?>
+            <aside id="aconnexion">
 
-			    <section id="fildariane">
-			         <i> Vous etes ici --> <a href="index.php">  Accueil </a></i>			    
-			    </section>
+              <h2 id="connexion"> Connexion </h2>
+              <form id="seconnecter" method="POST" action="connexion.php">
+              	<p>
+              	   <input type="text" name="pseudo" placeholder="Pseudo"/>
+              	</p>
+
+              	<p>
+                      <input type="password" name="password" placeholder="Mot de passe"/>
+              	</p>
+
+              	<p>
+              	   <input type="checkbox" name="souvenir"/> Se souvenir de moi 
+              	</p>
+
+              	<p>
+                     <input type="submit" value="Se connecter"/>
+              	</p>
+              </form>
+                <p id="oubli">
+                   <a href="oublie.php">Mot de passe perdu </a>
+                </p>
+                <p id="nouveau">
+                    <a href="register.php"> Nouveau , Inscivez Vous</a>
+                </p>
+
+                 </aside>
+              <?php 
+            }
+              ?>  
+              
+         
+
+          <div id="arianepresentation">
+
+      			    <section id="fildariane">
+      			         <i> Vous etes ici --> <a href="index.php">  Accueil </a></i>			    
+      			    </section>
 
 
 
-			    <section id="presentation">
+      			    <section id="presentation">
+                
+      			                <h1> SDS KEZAKO ? </h1>
+      			                <p>
+      			                    SDS ou plus communement Site Du Savoir est un site cumunautaire. Il a pour but de regrouper les informaticiens ( debutants , intermediaires , experts, confirmés) afin de partager nos Experiences , nos savoirs faire et nos astuces.
+      			                </p>
+
+      			                <h1 id="but"> De quoi est composé SDS </h1>
+
+      			                <p> 
+
+      			                <a href="#">Extras</a> : Section Pour l'ensemble des tutos hors informatique.<br />
+      					<a href="">Tutoriels</a> : Pour les tutos informatiques  Astuces( programmation, conception, partage d'experiences ).<br />
+                               		<a href="./Forum">Forum</a> : Pour tous les problèmes ou pour se présenter.<br />
+      	    				<a href="">Social</a> : pour se retrouver et discuter avec des inconnus une sorte de superchat global.
+
+      			                </p>
+
+      			    </section>
+           </div>
+
+           <div class="reste">
+              
+
+           </div>
           
-			                <h1> SDS KEZAKO ? </h1>
-			                <p>
-			                    SDS ou plus communement Site Du Savoir est un site cumunautaire. Il a pour but de regrouper les informaticiens ( debutants , intermediaires , experts, confirmés) afin de partager nos Experiences , nos savoirs faire et nos astuces.
-			                </p>
-
-			                <h1 id="but"> De quoi est composé SDS </h1>
-
-			                <p> 
-
-			                <a href="#">Extras</a> : Section Pour l'ensemble des tutos hors informatique.<br />
-					<a href="">Tutoriels</a> : Pour les tutos informatiques  Astuces( programmation, conception, partage d'experiences ).<br />
-                         		<a href="./Forum">Forum</a> : Pour tous les problèmes ou pour se présenter.<br />
-	    				<a href="">Social</a> : pour se retrouver et discuter avec des inconnus une sorte de superchat global.
-
-			                </p>
-
-			    </section>
-     </div>
-
-     <div class="reste">
-        
-
-     </div>
-    
-    <footer>
+          <footer>
+                
+                <p> Site du savoir Copyright &copy; 2016 Tous droits reservés </p>
           
-          <p> Site du savoir Copyright &copy; 2016 Tous droits reservés </p>
-    
 
-   </footer>
+         </footer>
 
-  
-     
-    
-         </div>
-</body>
+        
+           
+          
+               </div>
+      </body>
 </html>
--- reset.php
+++ reset.php
@@ -1,6 +1,7 @@
 
-<?php require_once './includes/identifiants.php'; ?>
-<?php include_once './includes/debut.php';
+<?php require_once './includes/identifiants.php';
+      include_once './includes/debut.php';
+
       include_once './includes/menu.php';
       include_once './includes/fonctions.php';
 
@@ -13,60 +14,59 @@
     if(isset($_GET['id'])  && isset($_GET['token']))
     {
        
+          $req = $bdd->prepare('SELECT * FROM membres WHERE membre_id = :id AND reset = :reset AND reset_at IS NOT NULL AND reset_at > DATE_SUB(NOW(), INTERVAL 30 MINUTE)');
 
-       $req = $bdd->prepare('SELECT * FROM membres WHERE membre_id = :id AND reset = :reset AND reset_at IS NOT NULL AND reset_at > DATE_SUB(NOW(), INTERVAL 30 MINUTE)');
+          $req->execute(array('id' => $_GET['id'], 'reset' => $_GET['token']));
 
-      $req->execute(array('id' => $_GET['id'], 'reset' => $_GET['token']));
+          $user = $req->fetch();
 
-      $user = $req->fetch();
+          if($user)
+          {
 
-      if($user)
-      {
+                if(!empty($_POST) && $_POST['password'] == $_POST['confirmation'])
 
-        if(!empty($_POST) && $_POST['password'] == $_POST['confirmation'])
+                {
+                      $password = PASSWORD_HASH($_POST['password'],PASSWORD_BCRYPT);
+                      $req = $bdd->prepare('UPDATE membres SET membre_mdp = :pass, reset = NULL, reset_at = NULL');
+                      $req->execute(array('pass'=> $pasword));
 
-        {
-          $password = PASSWORD_HASH($_POST['password'],PASSWORD_BCRYPT);
-          $req = $bdd->prepare('UPDATE membres SET membre_mdp = :pass, reset = NULL, reset_at = NULL');
-          $req->execute(array('pass'=> $pasword));
-
-          session_start();
-
-
-          $_SESSION['pseudo'] = $data['membre_pseudo'];
-          $_SESSION['level'] = $data['membre_rang'];
-          $_SESSION['id'] = $data['membre_id'];
-
-          $_SESSION['flash']['success'] = "Mot de passe mis a jours";
-
-         
-
-          header('Location: ./index.php');
-
-          exit();
+                      session_start();
 
 
-        }
+                      $_SESSION['pseudo'] = $data['membre_pseudo'];
+                      $_SESSION['level'] = $data['membre_rang'];
+                      $_SESSION['id'] = $data['membre_id'];
 
-      }
+                      $_SESSION['flash']['success'] = "Mot de passe mis a jours";
+
+                     
+
+                      header('Location: ./index.php');
+
+                      exit();
 
 
-      else
-      {
-        $_SESSION['flash'] = "Ce token n'est pas valide";
-        header('Location: connexion.php');
+                 }
 
-        exit();
+            }
 
-      }
+
+            else
+            {
+                  $_SESSION['flash'] = "Ce token n'est pas valide";
+                  header('Location: connexion.php');
+
+                  exit();
+
+            }
 
     }
 
     else
     {
 
-      header('Location: connexion.php');
-      exit();
+        header('Location: connexion.php');
+        exit();
 
     }
 
@@ -79,22 +79,21 @@
 
 <form action="" method="POST">
 
-    <div class="form-group">
-          <label for="pseudo">Mot de passe</label>
+            <div class="form-group">
 
-         <input type="password" name="password" class="form-control" />
-   </div>
+                  <label for="pseudo">Mot de passe</label>
+                  <input type="password" name="password" class="form-control" />
 
-   <div class="form-group">
-		   <label for="confirmation">Confirmation de Mot de passe </label>
-		   <input type="password" name="confirmation" class="form-control" />
-   </div>
+            </div>
 
-   
-   <button type="submit" class="btn btn-primary"> Envoyer </button>
-  
+            <div class="form-group">
 
+          		   <label for="confirmation">Confirmation de Mot de passe </label>
+          		   <input type="password" name="confirmation" class="form-control" />
 
+            </div>
 
-
+         
+            <button type="submit" class="btn btn-primary"> Envoyer </button>
+        
 </form>
