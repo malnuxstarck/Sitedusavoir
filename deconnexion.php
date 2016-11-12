d60892844adfcb37e9ef6939f7b4f1d30e5cb6ef
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2016-11-12 13:18:13 +0100

    Correction des erreurs lors de la deconnexion

--- deconnexion.php
+++ deconnexion.php
@@ -1,21 +1,30 @@
 <?php
 session_start();
 
+
 session_destroy();
 
 unset($_SESSION['flash']);
+unset($_COOKIE['souvenir']);
 
 setcookie('souvenir',NULL,-1);
+
 include("./includes/identifiants.php");
 include_once('./includes/debut.php');
 include_once('./includes/menu.php');
+
+
+
 
 $query=$bdd->prepare('DELETE FROM forum_whosonline WHERE online_id=
 :id');
 $query->bindValue(':id',$id,PDO::PARAM_INT);
 $query->execute();
 $query->CloseCursor();
-
+ 
+$req = $bdd->prepare('UPDATE membres SET cookie = NULL WHERE membre_id=:id');
+$req->execute(array('id'=>$id));
+$req->CloseCursor();
 
 if ($id == 0) 
 {
