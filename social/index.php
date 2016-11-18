50fbddddd5af8aa256cc5f3c1c019e28ee54bde9
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2016-11-18 14:11:58 +0100

    debut de la page index du reseau social

--- social/index.php
+++ social/index.php
@@ -1,3 +1,9 @@
 <?php
+  session_start();
+$titre="Social";
+include("../includes/identifiants.php");
+include("../includes/debut.php");
+include("../includes/menu.php");
+
      echo 'Sui le futur reseau social de sitedusavoir';
 ?>
