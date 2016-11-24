361d1fbe6c17d20c39518f9543d93e600b3d95a5
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2016-11-24 02:24:54 +0100

    Fonction pour deplacer un logo pour blog

--- includes/fonctions.php
+++ includes/fonctions.php
@@ -31,6 +31,26 @@ move_uploaded_file($avatar['tmp_name'],$name);
 }
 
 
+function move_logo($logo)
+{
+
+$extension_upload = strtolower(substr( strrchr($logo['name'],'.') ,1));
+
+$name = time();
+
+$nomlogo = str_replace(' ','',$name).".".$extension_upload;
+
+$name = "./logo/".str_replace('','',$name).".".$extension_upload;
+
+move_uploaded_file($logo['tmp_name'],$name);
+
+  return $nomlogo;
+
+}
+
+
+
+
 function str_random($nombre)
 {
 
@@ -54,24 +74,28 @@ function verif_auth($auth_necessaire)
 function get_list_page($page, $nb_page, $link, $nb = 2)
 {
 
-$list_page = array();
+        $list_page = array();
 
-for ($i=1; $i <= $nb_page; $i++){
-if (($i < $nb) OR ($i > $nb_page - $nb) OR (($i < $page + $nb) AND
-($i > $page -$nb)))
-$list_page[] = ($i==$page)?'<strong>'.$i.'</strong>':'<a
-href="'.$link.'&amp;page='.$i.'">'.$i.'</a>';
-else{
-if ($i >= $nb AND $i <= $page - $nb)
-$i = $page - $nb;
-elseif ($i >= $page + $nb AND $i <= $nb_page - $nb)
-$i = $nb_page - $nb;
-$list_page[] = '...';
+        for ($i=1; $i <= $nb_page; $i++)
+        {
+                if (($i < $nb) OR ($i > $nb_page - $nb) OR (($i < $page + $nb) AND ($i > $page -$nb)))
 
-}
-}
-$print= implode('-', $list_page);
-return $print;
+                $list_page[] = ($i==$page)?'<strong>'.$i.'</strong>':'<a href="'.$link.'&amp;page='.$i.'">'.$i.'</a>';
+
+                else
+                {
+                    if ($i >= $nb AND $i <= $page - $nb)
+                    $i = $page - $nb;
+                    elseif ($i >= $page + $nb AND $i <= $nb_page - $nb)
+                    $i = $nb_page - $nb;
+                    $list_page[] = '...';
+
+                }
+        }
+
+        $print= implode('-', $list_page);
+        
+        return $print;
 }
 
 
