0237f030f98d28312e984b2ddd5e10df6bd13a6d
Abdoul Malik <abdoulmalikhamidouidi@gmail.com>
2016-12-08 16:06:04 +0000

    Creation du fichier qui demarre la session

---
+++ includes/session.php
@@ -0,0 +1,18 @@
+// Demarrerla session dans une page 
+
+
+<?php
+
+
+$protocol = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
+
+if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
+    header('Location: '.$protocol.substr($_SERVER['HTTP_HOST'], 4).'/'.$_SERVER['REQUEST_URI']);
+    exit();
+}
+
+
+if(session_status()==PHP_SESSION_NONE)
+session_start(["cookie_domain" => ".sitedusavoir.com"]);
+
+?>
