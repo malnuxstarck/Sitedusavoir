ab2245ca8c14f129186cc917bce42bb00b336018
Shigehiro <Shiigehiro@users.noreply.github.com>
2016-12-17 00:27:13 +0100

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -18,7 +18,7 @@ if($user AND $user['token'] == $token)
 {
 	session_start();
 
-	$_SESSION['flash']['success'] =" votre compte a ete validee,Vous pouvez vous connecter maintenant";
+	$_SESSION['flash']['success'] = "Votre compte a été validé, vous pouvez vous connecter maintenant.";
 
 	$req = $bdd->prepare('UPDATE membres SET token = NULL, membre_inscrit = NOW() WHERE membre_id = :id ');
 
@@ -31,6 +31,6 @@ else
 {
 	header('Location:connexion.php');
 
-	$_SESSION['flash']['danger'] = "Cet Token n'est plus valide !!";
+	$_SESSION['flash']['danger'] = "Ce token n'est plus valide !!";
 
 }
