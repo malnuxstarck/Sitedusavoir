15dcefa27b54912856d656b545b198951c078b86
Shigehiro <Shiigehiro@users.noreply.github.com>
2016-12-17 01:11:16 +0100

    Update voirprofil.php

--- forum/voirprofil.php
+++ forum/voirprofil.php
@@ -25,9 +25,9 @@
     $query->execute();
     $data = $query->fetch();
     //On affiche les infos sur le membre
-    echo '<p id="fildariane"><i>Vous êtes ici</i> : <a href="./index.php">Forum</a> -->profil de '.stripslashes(htmlspecialchars($data['membre_pseudo']));
+    echo '<p id="fildariane"><i>Vous êtes ici</i> : <a href="./index.php">Forum</a> --> Profil de '.stripslashes(htmlspecialchars($data['membre_pseudo']));
     echo'<h1>Profil de '.stripslashes(htmlspecialchars($data['membre_pseudo'])).'</h1>';
-    echo'<img src="../images/avatars/'.$data['membre_avatar'].'" alt="Ce membre n a pas d avatar" />';
+    echo'<img src="../images/avatars/'.$data['membre_avatar'].'" alt="Ce membre n\'a pas d avatar" />';
     echo'<p><strong>Adresse E-Mail : </strong><a href="mailto:'.stripslashes($data['membre_email']).'">'.stripslashes(htmlspecialchars($data['membre_email'])).'</a><br />';
     echo'Ce membre est inscrit depuis le <strong>'.$data['membre_inscrit'].'</strong> et a posté <strong>'.$data['membre_post'].'</strong> messages<br /><br />';
     $query->closeCursor();
