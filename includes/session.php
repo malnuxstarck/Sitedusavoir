3037d1ffae0dd82df3b8d6cac3e7eb712a4ec73e
MalnuxStarck <abdoumalikhamidouidi@gmail.com>
2016-12-26 22:49:38 +0000

    Correction del'url de redirection dans session.php

--- includes/session.php
+++ includes/session.php
@@ -8,12 +8,12 @@
 $protocol = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
 
 if (substr($_SERVER['HTTP_HOST'], 0, 4) === 'www.') {
-    header('Location: '.$protocol.substr($_SERVER['HTTP_HOST'], 4).'/'.$_SERVER['REQUEST_URI']);
+    header('Location: '.$protocol.substr($_SERVER['HTTP_HOST'], 4).$_SERVER['REQUEST_URI']);
     exit();
 }
 
 
-if(session_status()==PHP_SESSION_NONE)
+if(session_status() == PHP_SESSION_NONE)
 session_start(["cookie_domain" => ".sitedusavoir.com"]);
 
 ?>
