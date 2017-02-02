9b7fdd3f6f88abf0f37ce173363a3fedabc3043e
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2017-02-02 17:23:27 +0100

    creation des fichiers pour social

--- social/index.php
+++ social/index.php
@@ -1,11 +1,93 @@
 <?php
 
-
-session_start();
-$titre="Social";
+$titre="Social | SiteduSavoir.com";
+include ("../includes/session.php");
 include("../includes/identifiants.php");
 include("../includes/debut.php");
 include("../includes/menu.php");
 
-     echo 'Sui le futur reseau social de sitedusavoir';
-?>
+if(!$id)
+{
+	$_SESSION['flash']['danger'] = " Vous devez etre connecté pour voir cette partie";
+	header('Location:../connexion.php');
+}
+
+$membre = $bdd->prepare('SELECT membre_pseudo , membre_avatar_mini
+	                     FROM membres WHERE membre_id = :id');
+$membre->bindParam(':id',$id,PDO::PARAM_INT);
+$membre->execute();
+
+$membre = $membre->fetch();
+
+echo '<aside class="aside-s">
+           <p>'.$membre['membre_pseudo'].'
+           </p>
+           <section>
+               <h3> Actualités </h3>
+           <ul>
+              <li> 
+                  <a href="./index.php">News</a>
+              <li>
+              <li>
+                   <a href="../membre/amis.php">Amis </a>
+              </li>
+              <li>
+                  <ahref="./notifications.php"> Notifications </a>
+              </li>
+           </ul>
+           </section>
+
+           <section class="groupes">
+               <h3> Groupes </h3>
+               <ul>';
+
+$listgroupes = $bdd->prepare('SELECT groupes_nom , social_groupes.groupes_id 
+	                          FROM social_groupes
+	                          JOIN social_gs_membres 
+	                          ON social_groupes.groupes_id  = social_gs_membres.groupes_id
+	                          WHERE membre_id = :id');
+$listgroupes->bindParam(':id',$id ,PDO::PARAM_INT);
+$listgroupes->execute();
+
+if($listgroupes->rowCount() > 0)
+{
+	$i = 0 ;
+
+   while($groupe = $listgroupes->fetch())
+   {
+	   	 $i++;
+	   	 echo '<li><a href="voirgroupe.php?g='.$groupe['groupes_id'].'">'.$groupe['groupes_nom'].'</a></li>';
+	   	 if($i > 10)
+	   	 {
+	   	 	echo '<li><a href="mesgroupes.php">Tous mes groupes </a></li>';
+	   	 	$groupe = NULL ;
+	   	 }
+   }
+}
+   echo'
+    </ul>
+    </section>
+
+</aside>
+<div class="fil">
+         <section class="top">
+                 <form action =filok.php" method="POST" enctype="multipart/form-data">
+
+                 <span class="avatar"><img src="../images/avatar_min/'.$membre['membre_avatar_mini'].'" alt="pas davatar"/></span>
+                 <div class="textarea">
+                    <textarea>Votre statut </textarea>
+                 </div>
+                 <div class="fichier">
+                       <input type="file" name="photo"/>
+                 </div>
+                 <div>
+                      <input type="submit" value="Statuer"/>
+                 </div>
+                    
+         </section>
+
+
+         <section class="actu">
+
+         </section>
+</div>';	                     
--- tutoriels/liretuto.php
+++ tutoriels/liretuto.php
@@ -5,17 +5,63 @@ include ("../includes/session.php");
 include("../includes/identifiants.php");
 include("../includes/debut.php");
 include("../includes/menu.php");
+?>
+ <p id="fildariane"><i><a href="../index.php">Accueil</a> --> <a href="./index.php">Tutoriels</a>--> Lire un tuto </i></p>
+<?php
 
 $tuto_id = (!empty($_GET['tuto']))?(int)$_GET['tuto']:1;
 
-
-$req = $bdd->prepare('SELECT tutos_titre FROM tutos WHERE tutos_id = :tuto');
+$req = $bdd->prepare('SELECT *
+	                  FROM tutos 
+	                  WHERE tutos_id = :tuto');
 
 $req->execute(array('tuto' => $tuto_id));
-
 $tuto = $req->fetch();
 
 
-echo $tuto['tutos_titre'];
+echo '<div class="tuto">
+           <section class="tuto-t">
+                <h2 class="titre_tuto">'.$tuto['tutos_titre'].'</h2>
+           </section>
+           <section class="intro-t">
+                    '.$tuto['tutos_intro'].'
+           </section>';
 
-?>
+   
+$partie = $bdd->prepare('SELECT * 
+	                     FROM tutos_parties 
+	                     WHERE tutos_id = :tuto
+	                     ORDER BY parties_id ');
+$partie->bindParam(':tuto',$tuto_id,PDO::PARAM_INT);
+$partie->execute();
+
+while($parties = $partie->fetch())
+{
+    echo '<section class="corps-t">
+                     <h3 class="titre-sec">'.$parties['parties_titre'].'</h3>
+                     <div class="contenu-part">
+                          '.$parties['parties_contenu'].'
+                     </div>
+     </section>';
+}
+         echo '<section class="intro-t">
+                    '.$tuto['tutos_conc'].'
+           </section>';
+
+ $auteur = $bdd->prepare('SELECT membre_pseudo , membres.membre_id 
+                          FROM membres
+                          LEFT JOIN tutos_par
+                          ON tutos_par.membre_id = membres.membre_id
+                          WHERE tutos_id = :tuto');
+
+$auteur->bindParam(':tuto',$tuto_id, PDO::PARAM_INT);
+
+$auteur->execute();
+echo'<ul class="auteur"> Par : ';
+
+while($auteurs = $auteur->fetch())
+{
+      echo '<li><a href="../forum/voirprofil.php?m='.$auteurs['membre_id'].'&action=consulter">'.$auteurs['membre_pseudo'].'</a></li>';
+}
+
+echo '</ul></div>';
