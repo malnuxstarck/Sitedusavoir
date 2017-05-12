af24b157a20f45400e5efa31dca719732dc49e38
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2017-05-12 20:58:26 +0200

    Ajout de quelques fonctionnalités sur la class ManagerMembre

--- class/ManagerMembre.class.php
+++ class/ManagerMembre.class.php
@@ -5,10 +5,13 @@
 class ManagerMembre
 {
 	const PSEUDO_LIBRE = 1 ;
-	const PSEUDO_OCCUPE = 2 ;
-	const MAIL_IBRE = 3 ;
-	const MAIL_OCCUPE = 4 ;
-	const
+	const PSEUDO_OCCUPE = 0 ;
+	const MAIL_LIBRE =  1;
+	const MAIL_OCCUPE = 0 ;
+	const CONNECTE = 1;
+	const NON_CONNECTE = 0 ;
+	const COMPTE_VALIDE = 1 ;
+	const COMPTE_NON_VALIDE = 0;
 	protected $db;
 
 	public function __construct(PDO $db)
@@ -16,13 +19,17 @@ class ManagerMembre
 		$this->setDb($db);
 	}
 
-	public function setDb(PDO $db)
+	public function setDb($db)
 	{
 		$this->db = $db ;
 	}
 
-	public function inscription()
+	public function inscription(Membre $membre)
 	{
+	    $erreurs = array();
+        
+
+
 
 
 	}
@@ -53,15 +60,33 @@ class ManagerMembre
 
     }
 
-	public function verifPseudo()
+
+	public function pseudoLibre(Membre $membre)
 	{
+        $pseudo = $membre->pseudo();
+        $req = $this->_db->prepare('SELECT COUNT(*) AS nbr FROM membres WHERE membre_pseudo = :pseudo');
+        $req->bindValue(':pseudo' ,$pseudo , PDO::PARAM_STR);
+        $req->execute();
+        $nombrePseudos = $req->fetch();
+        $nombrePseudos = $nombrePseudos['nbr'];
+
+        if($nombrePseudos == 1)
+            return ManagerMembre::PSEUDO_OCCUPE;
+        else
+            return ManagerMembre::PSEUDO_LIBRE;
 
 	}
 
-	public function pseudoLibre()
-	{
+	public function pseudoValide(Membre $membre)
+    {
+        $pseudo = $membre->pseudo();
+        $longeurP = strlen($pseudo);
 
-	}
+        if($longeurP < 3 || $longeurP > 15 || !preg_match("#^[a-zA-Z0-9_]+$"))
+            return 0;
+        else
+            return 1;
+    }
 
 	public function confirmerCompte()
 	{
