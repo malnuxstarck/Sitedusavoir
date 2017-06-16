cd5fb7b53a0ab9bb5729d921b1bb1fce8b249a3f
MalnuxStarck <abdoulmalikhamidouidi@gmail.com>
2017-06-17 00:11:21 +0200

    Premieres fonctions , et premieres proprietes pour la class Membre

--- class/ManagerMembre.class.php
+++ class/ManagerMembre.class.php
@@ -4,6 +4,149 @@
 
 class ManagerMembre
 {
+	// Les constantes de la classe 
+
+    const ERR_IS_CO =  'Vous etes deja connecter';
+    const ERR_IS_NOT_CO =  'Vous devez d\'abord vous connecter ';
+    const VALIDE = 1 ;
+    const NON_VALIDE = 0;
+
+	protected $_db;
+	protected $_errors = array();
+	protected $nombresErreurs = 0;
+
+	public function __construct(PDO $db)
+	{
+		$this->_db = $db ;
+	}
+ 
+	public function pseudoLibre($pseudo)
+	{
+		$req = $this->_db->prepare("SELECT COUNT(*) 
+			                 AS nbr 
+			                 FROM membres 
+			                 WHERE pseudo = :pseudo");
+		$req->bindValue(":pseudo",$pseudo,PDO::PARAM_STR);
+		$req->execute();
+		$nombresMembres = $req->fetchColumn();
+
+		if($nombresMembres != 0)
+		{
+			$this->nombresErreurs++;
+			$this->_errors["pseudo1"] = "Votre pseudo est dejas pris , nous sommes desolé ";
+			return NON_VALIDE ;
+		}
+
+		return VALIDE;
+
+	}
+
+	public function pseudoValide($pseudo)
+	{
+		if (strlen($pseudo) < 3 || strlen($pseudo) > 15  || !preg_match('#^[a-zA-Z0-9_]+$#',$pseudo))
+        {
+               $this->_errors["pseudo2"] = "Votre pseudo est soit trop grand, soit trop petit, soit il ne respecte les caracteres autorisées";
+               $this->nombresErreurs++;
+               return NON_VALIDE;
+        }
+
+        return VALIDE ;
+	}
+
+	public function emailValide($email)
+	{
+		$query = $this->_db->prepare('SELECT COUNT(*) AS nbr FROM membres WHERE membre_email =:mail');
+        $query->bindValue(':mail',$email, PDO::PARAM_STR);
+        $query->execute();
+        $nombresEmails = $query->fetchColumn();
+
+        if($nombresEmails != 0 || empty($email) || !filter_var($email,FILTER_VALIDATE_EMAIL))
+        {
+        	$this->nombresErreurs++;
+        	$this->_errors["mail1"] = " Votre email est soit deja prise , soit ne respecte pas le format ";
+        	return NON_VALIDE ;
+        }
+
+        return VALIDE;
+	}
+
+    public function verifyPassword(Membre $membreAInscrire)
+    {
+    	$password = $membreAInscrire->password();
+    	$confirmPassword = $this->confirmPassword();
+
+    	if($password != $confirmPassword || empty($confirmPassword) || empty($password))
+        {
+            $this->_errors["password"] = "Votre mot de passe et votre confirmation diffèrent, ou sont vides";
+           $this->nombresErreurs++;
+        }
+
+        else{
+
+	  	   $pass = PASSWORD_HASH($pass,PASSWORD_BCRYPT);
+	       $membreAInscrire->setPassword($pass);
+	       $membreAInscrire->setConfirmPassword(NULL);
+        }
+    }
+
+
+    public function verifAvatar($avatar)
+    {
+		if (!empty($avatar['size']))
+		  {
+		    //On définit les variables :
+
+		    $maxsize = 54000; //Poid de l'image
+		    $maxwidth = 180; //Largeur de l'image
+		    $maxheight = 180; //Longueur de l'image
+
+		    $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png', 'bmp' ); 
+		    //Liste des extensions valides
+
+		    if ($avatar['error'] > 0)
+		    {
+			      $this->_errors["avatar1"] = "Erreur lors du transfert de l'avatar : ";
+			      $this->nombresErreurs++;
+		    }
+
+		    if ($avatar['size'] > $maxsize)
+		    {
+		        $nombresErreurs++;
+		        $this->_errors["avatar2"] = "Le fichier est trop gros :(<strong>"$avatar['size']." Octets</strong> contre <strong>".$maxsize." Octets</strong>)";
+		    }
+
+		    $image_sizes = getimagesize($avatar['tmp_name']);
+
+		    if ($image_sizes[0] > $maxwidth OR $image_sizes[1] > $maxheight)
+		    {
+			      $nombresErreurs++;
+			      $this->_errors["avatar3"] = "Image trop large ou trop longue : (<strong>".$image_sizes[0]."x".$image_sizes[1]."</strong> contre
+			      <strong>".$maxwidth."x".$maxheight."</strong>)";
+		    }
+
+		    $extension_upload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.') ,1));
+
+		    if(!in_array($extension_upload,$extensions_valides) )
+		    {
+			      $nombresErreurs++;
+			      $this->_errors["avatar4"] = "Extension de l'avatar incorrecte";
+		    }
+
+		  }
+    }
+
+    public function envoyerMail($email,$titre,$message)
+    {
+         mail($email,$titre,$message);
+    }
+
+	public function inscription(Membre $membreAInscrire)
+	{
+
+
+	}
+
+
 	
 
 }
--- class/Membre.class.php
+++ class/Membre.class.php
@@ -6,6 +6,243 @@ Objet Membre
 
 class Membre
 {
+	// Les differents rangs qu'un membre peut avoir
+
+	const BANNI      = 0 ;
+	const VISITEUR   = 1 ;
+	const MEMBRE     = 2 ;
+	const MODO       = 3 ;
+	const ADMIN      = 4 ;
+
+	// Les attributs d'un membre
+
+	protected $_id ;
+	protected $_pseudo ;
+	protected $_password ;
+	protected $_confirmPassword ;
+    protected $_email ;
+    protected $_localisation ;
+    protected $_siteweb ;
+    protected $_visite ;
+    protected $_inscrit ;
+    protected $_token ;
+    protected $_reset ;
+    protected $_reset_at ;
+    protected $_signature ;
+    protected $_posts ;
+    protected $_rang ;
+    protected $_avatar ;
+    protected $_colkiee ;
+
+    // le constructeur 
+
+    public function __construct(array $membreA)
+    {
+    	$this->hydrate($membreA);
+    }
+
+    public function str_random($taille)
+    {
+    	$alphabet ="0123456789azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN";
+        return substr(str_shuffle(str_repeat($alphabet,$taille)) ,0, $taille);
+    }
+
+
+    // les getters  , qui renvoie la valeur d'un attribut 
+
+    public function id()
+    {
+    	return $this->_id ;
+    }
+    
+    public function pseudo()
+    {
+    	return $this->_pseudo ;
+    }
+
+    public function password()
+    {
+    	return $this->_password ;
+    }
+
+    public function confirmPassword()
+    {
+    	return $this->_confirmPassword ;
+    }
+
+    public function rang()
+    {
+    	return $this->_rang ;
+    }
+
+    public function email()
+    {
+    	return $this->_email ;
+    }
+
+    public function posts()
+    {
+    	return $this->_posts ;
+    }
+
+    public function avatar()
+    {
+    	return $this->_avatar ;
+    }
+
+    public function visite()
+    {
+    	return $this->_visite ;
+    }
+
+    public function token()
+    {
+    	return $this->_token ;
+    }
+
+    public function localisation()
+    {
+    	return $this->_localisation ;
+    }
+
+    public function cookiee()
+    {
+    	return $this->_colkiee ;
+    }
+
+    public function siteweb()
+    {
+    	return $this->_siteweb ; 
+    }
+
+    public function inscrit()
+    {
+    	return $this->_inscrit ;
+    }
+
+    public function signature()
+    {
+    	return $this->_signature ;
+    }
+
+    punlic function reset()
+    {
+    	return $this->_reset ;
+    }
+
+    public function reset_at()
+    {
+    	return $this->_reset_at ;
+    }
+
+    // Les setters , permetent de modifier le contenu ou la valeur d'un attribut ;
+
+
+    public function setId($id)
+    {
+    	if (is_int($id) AND $id > 0)
+    	{
+    		$this->_id = $id ;
+
+    	}
+    }
+
+    public function setPseudo($pseudo)
+    {
+       $this->_pseudo = $pseudo ;
+    }
+
+
+    public function setPassword()
+    {
+    	$this->_password = $password;
+    }
+
+    public function setConfirmPassword($confirmP)
+    {
+    	 $this->_confirmPassword = $confirmP;
+    }
+
+    public function setRang($rang)
+    {
+    	$this->_rang = $rang ;
+    }
+
+    public function setEmail($email)
+    {
+    	$this->_email = $email ;
+    }
+
+    public function setPosts($posts)
+    {
+    	 $this->_posts = $posts
+    }
+
+    public function setAvatar($avatar)
+    {
+    	 $this->_avatar = $avatar;
+    }
+
+    public function setVisite($visite)
+    {
+    	$this->_visite = $visite ;
+    }
+
+    public function setToken($token)
+    {
+    	$this->_token = $token ;
+    }
+
+    public function setLocalisation($localisation)
+    {
+    	 $this->_localisation = $localisation ;
+    }
+
+    public function setCookiee($cookiee)
+    {
+    	 $this->_colkiee = $cookiee ;
+    }
+
+    public function setSiteweb($siteweb)
+    {
+    	 $this->_siteweb = $siteweb ; 
+    }
+
+    public function setInscrit($inscrit)
+    {
+    	 $this->_inscrit = $inscrit ;
+    }
+
+    public function setSignature($signature)
+    {
+    	 $this->_signature = $signture ;
+    }
+
+    public function setReset($reset)
+    {
+    	 $this->_reset = $reset ;
+    }
+
+    public function setReset_at($date)
+    {
+    	 $this->_reset_at = $date ;
+    }
+
+
+  public function hydrate(array $membreA)
+  {
+  	foreach ($membreA AS $cle => $contenu)
+  	{
+  		$method = 'set'.ucfirst($le);
+  		if(method_exists($this, $method))
+  		{
+  			$method($contenu);
+  		}
+  	}
+  }
+
+
+
 	
 
 }
